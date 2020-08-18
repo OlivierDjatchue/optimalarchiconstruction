c3bf2a6d613783f2ab660da4874893709e045490
OlivierDjatchue <olivierdja@yahoo.fr>
2020-08-18 14:52:34 +0200

    erste version fertig

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-require('bootstrap')
+const navbar = document.getElementById('navigation');
+const colorHeading = document.querySelector('.heading-primary-main');
+
+window.onscroll = function() {
+	scroll();
+};
+
+function scroll() {
+	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+		navbar.classList.add('bg-dark');
+	} else {
+		navbar.classList.remove('bg-dark');
+		colorHeading.style.color = 'white';
+	}
+}
+
+// $(document).ready(function() {
+// 	// Add smooth scrolling to all links
+// 	$('a').on('click', function(event) {
+// 		// Make sure this.hash has a value before overriding default behavior
+// 		if (this.hash !== '') {
+// 			// Prevent default anchor click behavior
+// 			event.preventDefault();
+
+// 			// Store hash
+// 			var hash = this.hash;
+
+// 			// Using jQuery's animate() method to add smooth page scroll
+// 			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+// 			$('html, body').animate(
+// 				{
+// 					scrollTop: $(hash).offset().top
+// 				},
+// 				800,
+// 				function() {
+// 					// Add hash (#) to URL when done scrolling (default click behavior)
+// 					window.location.hash = hash;
+// 				}
+// 			);
+// 		} // End if
+// 	});
+// });
